40ac8e5bffda0a70a81b9ac6bd2c752ec266f3d0
Godwin2603 <ukpojuwin@gmail.com>
2025-11-28 12:01:43 +0100

    Learned about http methods

--- express_app/server.js
+++ express_app/server.js
@@ -1,12 +1,43 @@
 const express = require('express'); 
+const path = require("path")
+
 const app = express();             
 
+
+//serve static file
+// app.use(express.static(path.join(__dirname, "")))
+
+//middleware
+app.use(express.urlencoded({extended: true}))
+
+
+//Routes
 app.get('/', (req, res) => {       
-  res.send('Welcome to Express.');  
+  res.sendFile(path.join(__dirname, 'templates/index.html'))  
 });
 
 app.get('/about', (req,res) => {
-    res.send('Welcome to about')
+    res.sendFile(path.join(__dirname, 'templates/about.html'))  
+})
+app.get('/signup', (req,res) => {
+    res.sendFile(path.join(__dirname, 'templates/signup.html'))  
+})
+
+//post
+app.post('/signup', (req,res) => {
+  res.send('Welcome to signup')
+})
+
+
+//put
+
+//patch
+
+//delete
+
+//404 handler
+app.use((req,res) =>{
+  res.status(404).sendFile(path.join(__dirname, 'templates/not-found.html'))  
 })
 
 app.listen(3000, () => {
